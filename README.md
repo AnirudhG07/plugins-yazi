8abdaab9bad4e245c1ec1970730e25414a0e9b30
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-06-27 00:10:13 +0530

    basic implementation and readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # cheatshh.yazi
-Save your commands in yazi-cheatshh, simply open it, copy any command and run it in your shell.
+A Yazi plugin for searching commands and copying them to your clipbaord, using [cheatshh](https://github.com/AnirudhG07/cheatshh)
+
+## Dependencies
+- [cheatshh](https://github.com/AnirudhG07/cheatshh)
+
+This is mainly for Unix(Linux and Macos). 
+
+## Installation
+Use the below command to install the plugin-
+```bash
+git clone https://github.com/AnirudhG07/cheatshh.yazi.git ~/.config/yazi/plugins/cheatshh.yazi
+```
+
+## Keymapping and Usage
+You can copy the below commands to `keymap.toml` to use the plugin-
+```toml
+[[manager.prepend_keymap]]
+on   = [ "c","h" ]
+run  = "plugin cheatshh"
+desc = "Find command in cheatshh"
+
+[[manager.prepend_keymap]]
+on   = [ "c","A" ]
+run  = "plugin cheatshh --args='a'"
+desc = "Add command in cheatshh"
+```
---
+++ init.lua
@@ -0,0 +1,50 @@
+local function splitAndGetFirst(inputstr, sep)
+	if sep == nil then
+		sep = "%s"
+	end
+	local sepStart, sepEnd = string.find(inputstr, sep)
+	if sepStart then
+		return string.sub(inputstr, 1, sepStart - 1)
+	end
+	return inputstr
+end
+
+local state = ya.sync(function()
+	return tostring(cx.active.current.cwd)
+end)
+
+local function fail(s, ...)
+	ya.notify({ title = "YaziCheatshh", content = string.format(s, ...), timeout = 5, level = "error" })
+end
+
+local function entry(_, args)
+	local _permit = ya.hide()
+	local cwd = state()
+	local shell_value = os.getenv("SHELL"):match(".*/(.*)")
+	local cmd_args = ""
+	if args[1] == nil then
+		cmd_args = [[cheatshh]]
+	elseif args[1] == "a" then
+		cmd_args = [[cheatshh -a]]
+	end
+	local child, err = Command(shell_value)
+		:args({ "-c", cmd_args })
+		:cwd(cwd)
+		:stdin(Command.INHERIT)
+		:stdout(Command.PIPED)
+		:stderr(Command.INHERIT)
+		:spawn()
+
+	if not child then
+		return fail("Spawn `cheatshh` failed with error code %s. Do you have it installed?", err)
+	end
+
+	local output, err = child:wait_with_output()
+	if not output then
+		return fail("Cannot read `cheatshh` output, error code %s", err)
+	elseif not output.status.success and output.status.code ~= 130 then
+		return fail("`cheatshh` exited with error code %s", output.status.code)
+	end
+end
+
+return { entry = entry }
