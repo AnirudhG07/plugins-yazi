08cf70bd0cff1d4137c807741c05137ff48b07d7
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-07-04 00:53:50 +0530

    options command
    very buggy, though its problem of cheatshh instead of yazi

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
 # cheatshh.yazi
+
 A Yazi plugin for searching commands and copying them to your clipbaord, using [cheatshh](https://github.com/AnirudhG07/cheatshh)
 
 ## Dependencies
+
 Please follow the download instruction and dependencies in the below link.
+
 - [cheatshh](https://github.com/AnirudhG07/cheatshh)
 
-This is mainly for Unix(Linux and Macos). 
+This is mainly for Unix(Linux and Macos).
 
 ## Installation
+
 Use the below command to install the plugin-
+
 ```bash
 git clone https://github.com/AnirudhG07/cheatshh.yazi.git ~/.config/yazi/plugins/cheatshh.yazi
 ```
+
 ## Keymapping and Usage
+
 You can copy the below commands to `keymap.toml` to use the plugin-
+
 ```toml
 [[manager.prepend_keymap]]
 on   = [ "c","h" ]
@@ -21,6 +29,21 @@ run  = "plugin cheatshh"
 desc = "Find command in cheatshh"
 ```
 
-## TODO
-1. Add provision for more flags in cheatshh hopefully using popup in the middle of the screen.
-2. Fill the yazi_cheatsheet with commands.
+To choose various options within Yazi.
+
+```toml
+[[manager.prepend_keymap]]
+on   = [ "c","H" ]
+run  = "plugin cheatshh --args=options"
+desc = "Find command in cheatshh"
+```
+
+**NOTE**
+The `options` flag has bugs. It is recommended not to use it. For an empty screen you see by chance, Please press Enter to Continue ahead.
+
+Instead to add,edit,delete commands and groups, either use the regular terminal or manually edit it in `$HOME/.config/cheatshh` direct:ory.
+
+## Bugs
+
+1. The flag command run within yazi do not run properly. They skip the whiptail asking for yes/no.
+
--- init.lua
+++ init.lua
@@ -3,18 +3,43 @@ local state = ya.sync(function()
 end)
 
 local function fail(s, ...)
-	ya.notify({ title = "YaziCheatshh", content = string.format(s, ...), timeout = 5, level = "error" })
+	ya.notify({ title = "Cheatshh", content = string.format(s, ...), timeout = 5, level = "error" })
 end
 
-local function entry(_, args)
-	local _permit = ya.hide()
+local function get_selected_option()
+    local command = [[
+    bash -c '
+    OPTIONS=("1" "Add command" "2" "Add group" "3" "Edit Command" "4" "Edit group" "5" "Delete Command" "6" "Delete group")
+    CHOICE=$(whiptail --title "Menu" --menu "Choose an option" 15 60 6 \
+    "1" "Add command" \
+    "2" "Add group" \
+    "3" "Edit Command" \
+    "4" "Edit group" \
+    "5" "Delete Command" \
+    "6" "Delete group" 3>&1 1>&2 2>&3)
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+        echo $CHOICE
+    else
+        echo "Cancelled"
+        exit 1
+    fi
+    '
+    ]]  -- Command to run your Bash script embedded as a string
+    local handle = io.popen(command, 'r')  -- Open the process for reading
+    if handle then
+        local output = handle:read("*a")  -- Read the entire output of the command
+        handle:close()  -- Close the process
+        return output  -- Return the captured output
+    else
+		fail("Failed to run cheatshh options")
+        return nil
+    end
+end
+
+local function commad_runner(cmd_args)
 	local cwd = state()
 	local shell_value = os.getenv("SHELL"):match(".*/(.*)")
-	local cmd_args = ""
-	if args[1] == nil then
-		cmd_args = [[cheatshh]]
-	-- All the flags are -a, -g, -ec, -eg, -dc, -dg, -m, simply we have to run cheatshh -flag.
-	end
 	local child, err = Command(shell_value)
 		:args({ "-c", cmd_args })
 		:cwd(cwd)
@@ -24,14 +49,45 @@ local function entry(_, args)
 		:spawn()
 
 	if not child then
-		return fail("Spawn `cheatshh` failed with error code %s. Do you have it installed?", err)
+		return fail("Spawn `cheatshh` failed with error code %s. Do you have `tag` installed?", err), child
 	end
 
 	local output, err = child:wait_with_output()
 	if not output then
-		return fail("Cannot read `cheatshh` output, error code %s", err)
+		return fail("Cannot read `cheatshh` output, error code %s", err), output
 	elseif not output.status.success and output.status.code ~= 130 then
-		return fail("`cheatshh` exited with error code %s", output.status.code)
+		return fail("`cheatshh` exited with error code %s", output.status.code), output
+    else
+        return true, output
+    end
+end
+
+local function entry(_, args)
+	local _permit = ya.hide()
+	local cmd_args = ""
+
+	local option_to_cmd_args = {
+		[1] = "cheatshh -a",
+		[2] = "cheatshh -g",
+		[3] = "cheatshh -ec",
+		[4] = "cheatshh -eg",
+		[5] = "cheatshh -dc",
+		[6] = "cheatshh -dg",
+	}
+
+	if args[1] == nil then
+		cmd_args = [[cheatshh]]
+	elseif args[1] == "options" then
+		local selected_option, err = get_selected_option()
+		cmd_args = option_to_cmd_args[selected_option] or nil
+
+		if not cmd_args then
+			return
+		end
+	end
+	local success, output = commad_runner(cmd_args)
+	if not success then
+		return
 	end
 end
 
