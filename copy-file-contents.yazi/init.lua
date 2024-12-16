11167e1860b10f885814ced0f04785ffa931d6c5
Anirudh Gupta <146579014+AnirudhG07@users.noreply.github.com>
2024-12-16 17:46:22 +0530

    feat: use ya.clipboard() for copying file contents (#2)
    * feat: use ya.clipboard() for copying file contents
    
    * add no of files copied msg

--- copy-file-contents.yazi/init.lua
+++ copy-file-contents.yazi/init.lua
@@ -17,20 +17,6 @@ local function notify(str)
 	})
 end
 
--- Mapping for OS and its corresponding content copy command
-local OS_clipboard_mapping = {
-	["linux"] = "xclip -selection clipboard",
-	["macos"] = "pbcopy",
-	["windows"] = "clip",
-	["ios"] = "pbcopy",
-	["freebsd"] = "xclip -selection clipboard",
-	["dragonfly"] = "xclip -selection clipboard",
-	["netbsd"] = "xclip -selection clipboard",
-	["openbsd"] = "xclip -selection clipboard",
-	["solaris"] = "xclip -selection clipboard",
-	["android"] = "termux-clipboard-set",
-}
-
 local state_option = ya.sync(function(state, attr)
 	return state[attr]
 end)
@@ -42,8 +28,7 @@ local function entry()
 		return
 	end
 	-- call the attributes from setup
-	local append_char, notification, clipboard_cmd =
-		state_option("append_char"), state_option("notification"), state_option("clipboard_cmd")
+	local append_char, notification = state_option("append_char"), state_option("notification")
 
 	local text = ""
 	for i, file in ipairs(files) do
@@ -60,19 +45,11 @@ local function entry()
 		end
 	end
 
-	local cmd_args = "echo " .. ya.quote(text) .. " | " .. clipboard_cmd
-	local shell_value = os.getenv("SHELL"):match(".*/(.*)")
-
-	-- Spawn the command to copy the file contents to clipboard
-	local output, err = Command(shell_value):args({ "-c", cmd_args }):spawn():wait()
-
-	if not output then
-		return ya.err("Cannot spawn clipboard command, error code " .. tostring(err))
-	end
-
+	-- Copy the file contents to clipboard
+	ya.clipboard(text)
 	-- Notify the user that the file contents have been copied to clipboard
 	if notification then
-		notify("Copied file contents to clipboard")
+		notify("Copied " .. #files .. " file(s) contents to clipboard")
 	end
 end
 
@@ -82,11 +59,6 @@ return {
 		state.append_char = options.append_char or "\n"
 		-- Enable notification
 		state.notification = options.notification and true
-		-- Set the clipboard command based on the OS
-		state.clipboard_cmd = options.clipboard_cmd or OS_clipboard_mapping[ya.target_os()]
-		if state.clipboard_cmd:lower() == "default" or state.clipboard_cmd == nil or state.clipboard_cmd == "" then
-			state.clipboard_cmd = OS_clipboard_mapping[ya.target_os()]
-		end
 	end,
 	entry = entry,
 }
